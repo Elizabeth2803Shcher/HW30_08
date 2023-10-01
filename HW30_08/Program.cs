762e865cf488ccb17cd4eccb089d04907639bca7
Elizabeth2803 <elizaart2801@gmail.com>
2023-10-01 21:53:14 +0200

    Добавьте файлы проекта.

---
+++ HW30_08/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+long inhabitantPiter = 5598486;
+long inhabitantMosckow = 13015126;
+long res = inhabitantMosckow - inhabitantPiter;
+Console.WriteLine($"В москве больше жителей на {res}");
+
+//Вторая задача.
+decimal incomeYear = 260548.56895m;
+decimal incomeMonth = incomeYear / 12;
+decimal Ndfl = incomeMonth / 100 * 13;
+decimal incomeMonthNdfl = incomeMonth - Ndfl;
+Console.WriteLine($"Работник зарабатывает в месяц {incomeMonth:F5} а с НДФЛ {incomeMonthNdfl:F5}");
